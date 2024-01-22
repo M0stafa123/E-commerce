e0c0b9a6e5053605961d714b4d26feef4917cc78
M0stafa123 <jodat.ahmed135@gmail.com>
2024-01-22 20:36:19 +0200

    updatet all.ts

--- server/src/all.ts
+++ server/src/all.ts
@@ -5,11 +5,21 @@ export const all = express.Router();
 all.get("/", (req, res) => {
   res.send({
     users: {
-      "/users": ["GET", "Delete  'will delete all users for development'"],
+      "/users": ["Delete  'will delete all users for development'"],
+      "/users/find": ["GET", "Will get all users", "FOR ADMINS ONLY"],
+      "/users/find/:id": [
+        "GET",
+        "will get one user by id",
+        "FOR AUTHENTICATED USERS AND ADMIN",
+      ],
+      "/users/:id": {
+        PUT: ["will update user info", "FOR AUTHENTICATED USERS ONLY"],
+        DELETE: ["will delete user by id", "FOR AUTHENTICATED USERS ONLY"],
+      },
     },
     auth: {
       "/auth/register": "POST",
-      "/auth/login": "POST",
+      "/auth/login": ["POST", "will return authentcation token"],
     },
   });
 });
