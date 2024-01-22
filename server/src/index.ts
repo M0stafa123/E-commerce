9d2085aabe6812af2215f6a069b9f510779c1575
M0stafa123 <jodat.ahmed135@gmail.com>
2024-01-22 12:22:25 +0200

    test

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,16 @@
 import express from "express";
 import cors from "cors";
-import { MongoClient } from "mongodb";
-import Router from "./routes";
+// import { MongoClient } from "mongodb";
+// import Router from "./routes";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
-app.use(Router);
+// app.use(Router);
 
 const url = "mongodb://0.0.0.0:27017/E-commerce";
-const Client = new MongoClient(process.env.URL || url);
+// const Client = new MongoClient(process.env.URL || url);
 app.get("/", (_req, res) => {
   res.send("connected successfully");
 });
