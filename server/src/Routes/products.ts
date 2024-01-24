1a12a0d5511d867e8e1282ad569563876e8c8c79
M0stafa123 <jodat.ahmed135@gmail.com>
2024-01-24 22:52:04 +0200

    handled errors

--- server/src/Routes/products.ts
+++ server/src/Routes/products.ts
@@ -6,34 +6,52 @@ const productRoute = express.Router();
 productRoute.use(bodyParser.json());
 // create product
 productRoute.post("/", verifyAdmin, async (req, res) => {
-  const create = await ProductModel.create(req.body);
-  res.status(201).json(create);
+  try {
+    const create = await ProductModel.create(req.body);
+    res.status(201).json(create);
+  } catch (err: any) {
+    if (err.code === 11000) {
+      res.status(500).json("same product is already present");
+    }
+  }
 });
 // find one or all products
 productRoute.get("/find/:id?", async (req, res) => {
-  if (req.params.id) {
-    const find = await ProductModel.findById(req.params.id);
-    res.status(200).json(find);
-  } else {
-    const findall = await ProductModel.find({});
-    res.status(200).json(findall);
+  try {
+    if (req.params.id) {
+      const find = await ProductModel.findById(req.params.id);
+      res.status(200).json(find);
+    } else {
+      const findall = await ProductModel.find({});
+      res.status(200).json(findall);
+    }
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 //  update product
 productRoute.put("/:id", verifyAdmin, async (req, res) => {
-  const update = await ProductModel.findByIdAndUpdate(
-    req.params.id,
-    {
-      $set: req.body,
-    },
-    { new: true }
-  );
-  res.status(200).json(update);
+  try {
+    const update = await ProductModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(update);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 //  delete product
 productRoute.delete("/:id", verifyAdmin, async (req, res) => {
-  const destroy = await ProductModel.findByIdAndDelete(req.params.id);
-  res.status(200).json("deleted successfully");
+  try {
+    const destroy = await ProductModel.findByIdAndDelete(req.params.id);
+    res.status(200).json("deleted successfully");
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // delete all for development
